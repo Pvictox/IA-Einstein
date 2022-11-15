1494acc82fc0970b6db956b4dfe439806aa16dd9
Pedro <paf.victor@gmail.com>
2022-11-15 13:26:04 -0300

    Algumas funções fitness

--- fitness.py
+++ fitness.py
@@ -2,6 +2,7 @@ from cromossomo import Cromossomo
 from conjEnum.enumNacionalidade import enumNac
 from conjEnum.enumCor import enumCor
 from conjEnum.enumPet import enumPet
+from conjEnum.enumBebida import enumBebida
 
 def calcularFitness(cromossomo):
     # 1) O norueguês vive na primeira casa
@@ -22,5 +23,21 @@ def calcularFitness(cromossomo):
             if (enumPet(cromossomo.getCor()[indexDaCasa]).name == "CACHORROS"):
                 cromossomo.addPonto()
     
+    # 4) O dinamarquês bebe chá
+    for num_Nacionalidade in cromossomo.getNacionalidade():
+        if (enumNac(num_Nacionalidade).name == "DINAMARQUES"):
+            indexDaCasa = cromossomo.getNacionalidade().index(num_Nacionalidade)
+            if (enumBebida(cromossomo.getCor()[indexDaCasa]).name == "CHA"):
+                cromossomo.addPonto()
+    
+    # 5) A casa VERDE fica ao lado esquedo da casa branca
+        # Se a casa verde for a de num 5 (indice == 4) indica que não tem como ela ficar a esquerda da branca pq não tem mais casas
+        for num_Cor in cromossomo.getCor():
+            if (enumCor(num_Cor).name == "VERDE"):
+                indexDaCasa = cromossomo.getCor().index(num_Nacionalidade)
+                if ( indexDaCasa != 4): #Existe casa a direita
+                    if (enumCor(indexDaCasa+1).name == "BRANCA"):
+                        cromossomo.addPonto()
+                    
     
             
